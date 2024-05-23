af6942decf726041f82e0083615e624064f246c5
Rishav Raj <rishavraj1707music@gmail.com>
2024-05-23 15:33:59 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "My Vite PWA",
+        short_name: "Awesome PWA",
+        description: "My PWA App description",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/maskable_icon.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#f0e7db",
+        display_override: ["fullscreen", "minimal-ui"],
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        id: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
+});
